806bbcf9e076e22e43608c1a8520f6518f7f6ede
D4n1Ela <dana@correoFalso.com>
2018-05-20 01:43:39 -0300

    'routes' y App.js: Creo el directorio 'routes' con los archivos de configuración y los importo en App.js.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component , Fragment} from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Fragment>
+          <Link to="/">Go to Home</Link>
+          
+          <Link to="/about">Go to About</Link>
+          <Route exact path="/" component={ Home }></Route>
+          <Route path="/about" component={ About }></Route>
+        </Fragment>
+      </Router>
     );
   }
 }
