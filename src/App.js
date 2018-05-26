e3b73e1f102862316a8c62ddd727e7e6d0a0d592
D4n1Ela <dana@correoFalso.com>
2018-05-26 01:40:54 -0300

    Agrego el link Contact. Además importo la ruta de Contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component , Fragment} from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
+import Contact from './routes/Contact';
 import './css/App.css';
 
 class App extends Component {
@@ -12,10 +13,12 @@ class App extends Component {
           <main>  
             <Route exact path="/" component={ Home }></Route>
             <Route path="/about" component={ About }></Route>
+            <Route path="/contact" component={ Contact }></Route>
           </main>
           <nav>
             <Link to="/"><span>Go to Home</span></Link>
             <Link to="/about"><span>Go to About</span></Link>
+            <Link to="/contact"><span>Go to Contact</span></Link>
           </nav>
         </Fragment>
       </Router>
