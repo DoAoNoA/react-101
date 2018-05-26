5d6b9ab3dd2b97111c47e477183a10326111c86c
D4n1Ela <dana@correoFalso.com>
2018-05-26 01:43:25 -0300

    Agrego el archivo routes que mostrará el link al componente MyContact.

---
+++ src/routes/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyContact from '../components/MyContact';
+import '../css/Contact.css'
+
+const Contact = () =>
+    <div className="MyContactContainer">
+        <MyContact/>
+    </div>
+
+export default Contact;
