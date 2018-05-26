1d7b0402465c16f04f0fa8c14b58c9dd177be816
D4n1Ela <dana@correoFalso.com>
2018-05-25 22:30:15 -0300

    Agrego un nuevo componente.

---
+++ src/components/AboutMe.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AboutMe extends Component{
+    render(){
+        return (
+          <div>
+              <h1>About me</h1>
+              <p>Programadora con un gran interés en proyectos desafiantes que motiven a aprender nuevas tecnologías.</p> 
+              <p>Mi objetivo es desarrollar productos IT de calidad con las mejores prácticas de programación.</p>
+              <p>Siempre dispuesta a compartir conocimiento, a colaborar y trabajar en equipo.</p>
+              <p>Autodidacta porque me gustan los desafíos y apasionada porque me entusiasma detectar soluciones.</p>
+          </div>
+        )
+    }
+}
+
+export default AboutMe;
