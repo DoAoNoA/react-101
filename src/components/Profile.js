05182a944d27dabbcc7ea39a96752b8a1f050d11
D4n1Ela <dana@correoFalso.com>
2018-05-25 22:43:09 -0300

    Modifico el nombre que le asigno a la variable que importa la foto de perfil.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../imgs/profilePicture.jpg';
+import profilePicture from '../imgs/profilePicture.jpg';
 
 class Profile extends Component{
     render(){
         return (
           <div>
-              <img src={logo} alt="logo"/>
+              <img src={profilePicture} alt="logo"/>
               <p>Full Stack in traninig</p>
           </div>
         )
